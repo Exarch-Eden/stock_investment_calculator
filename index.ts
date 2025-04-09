8a66ac71a54f9ffc57ca0501a0954043b2887c64
Exarch-Eden <56244072+Exarch-Eden@users.noreply.github.com>
2025-04-09 04:01:14 -0700

    Updated index file
    Implemented calculateDailyAmountYieldCurrencyMedianPriceWeighted helper function
    
    Now reads parameters with readStocksFromFile helper function instead of parseArgs

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
 import { parseArgs } from "./parse";
-import { calculateDailyAmountYieldAndCurrencyWeighted } from "./calculator";
+import { calculateDailyAmountYieldAndCurrencyWeighted, calculateDailyAmountYieldCurrencyMedianPriceWeighted } from "./calculator";
 import { writeToFileAsync } from "./write";
 import { readStocksFromFile } from "./read";
 
-const { balance, timestamp, cadWeight, usdWeight } = parseArgs();
+// const { balance, timestamp, cadWeight, usdWeight } = parseArgs();
+const { stocks, balance, timestamp, cadWeight, usdWeight } = readStocksFromFile("./input.json")
 
 writeToFileAsync(
     "./result.json",
     JSON.stringify(
-        calculateDailyAmountYieldAndCurrencyWeighted(
-            readStocksFromFile("./input.json"),
+        calculateDailyAmountYieldCurrencyMedianPriceWeighted(
+            stocks,
             balance,
             cadWeight,
             usdWeight,
