538619a6171d974ac0eb3f3c54187ce1b2e9f664
Exarch-Eden <56244072+Exarch-Eden@users.noreply.github.com>
2025-04-08 19:51:58 -0700

    Updated calculateDailyAmount helper function
    Implemented getRemainingWorkDaysAnnual helper function and replaced old associated logic

--- calculator.ts
+++ calculator.ts
@@ -1,25 +1,8 @@
+import { getRemainingWorkDaysAnnual } from "./date";
+import { Stock } from "./types";
+
 const calculateDailyAmount = (numStocks: number, annualBalance: number, startTimestamp?: number) => {
-    const curDate = new Date()
-
-    // Dec 31 at 23:59:59.999
-    const yearEndTimestamp = new Date(curDate.getFullYear(), 11, 31, 23, 59, 59, 999).getTime()
-
-    // TESTING
-    console.log("yearEndTimestamp: ", yearEndTimestamp);
-
-    // timestamp difference between today and last day of the year
-    const diffTimestamp = yearEndTimestamp - (startTimestamp || curDate.getTime())
-
-    const diffDays = Math.floor(diffTimestamp / (3600 * 24 * 1000))
-
-    // TESTING
-    console.log("diffDays: ", diffDays);
-
-    // does not account for holidays
-    const numWorkDaysRemaining = Math.floor(diffDays * (5/7))
-
-    // TESTING
-    console.log("num work days remaining: ", numWorkDaysRemaining);
+    const numWorkDaysRemaining = getRemainingWorkDaysAnnual(startTimestamp)
 
     return (annualBalance / (numStocks * numWorkDaysRemaining)).toFixed(2)
 }
