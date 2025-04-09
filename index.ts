e19a6a16022edf2ec999b9e0b53f304f7615de98
Exarch-Eden <56244072+Exarch-Eden@users.noreply.github.com>
2025-04-08 21:56:01 -0700

    Updated index file
    Removed destructured stocks argument from parseArgs implementation
    
    Implemented calculateDailyAmountYieldAndCurrencyWeighted helper function

--- index.ts
+++ index.ts
@@ -2,25 +2,32 @@ import { parseArgs } from "./parse";
 import {
     calculateDailyAmount,
     calculateDailyAmountWeighted,
+    calculateDailyAmountYieldAndCurrencyWeighted,
     calculateDailyAmountYieldWeighted,
 } from "./calculator";
 import { writeToFileAsync } from "./write";
 import { readStocksFromFile } from "./read";
 
-const { stocks, balance, timestamp } = parseArgs();
+const { balance, timestamp, cadWeight, usdWeight } = parseArgs();
 
 // writeToFileAsync(
 //     "./result.txt",
 //     calculateDailyAmount(stocks, balance, timestamp)
 // );
 
+// TESTING
+console.log("cadWeight: ", cadWeight);
+console.log("usdWeight: ", usdWeight);
+
 writeToFileAsync(
     "./result.json",
     JSON.stringify(
         // calculateDailyAmountWeighted(
-        calculateDailyAmountYieldWeighted(
+        calculateDailyAmountYieldAndCurrencyWeighted(
             readStocksFromFile("./input.json"),
             balance,
+            cadWeight,
+            usdWeight,
             timestamp
         )
     )
